1e5f9b23fe78ba16bce3693a2ca4b7d267b13ebb
GWturbina <shulgagrigory65@gmail.com>
2026-02-07 14:46:27 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
-
 module.exports = nextConfig
