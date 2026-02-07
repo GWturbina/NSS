49e135e9dd8ecf2c0102d9309310f0349dba58f0
GWturbina <shulgagrigory65@gmail.com>
2026-02-07 13:42:53 +0200

    Create  index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { Header } from './Header';
+export { BottomNav } from './BottomNav';
+export { Stone } from './Stone';
+export { EnergyBar } from './EnergyBar';
+export { RoadmapModal } from './RoadmapModal';
+export { ThemeToggle } from './ThemeToggle';
+export { ThemeProvider } from './ThemeProvider';
+export { LanguageSwitcher } from './LanguageSwitcher';
+export { TelegramProvider, useTelegramContext, useTelegramSafe } from './TelegramProvider';
